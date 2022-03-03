6cb756eb529a3c6a1017aaa2df64890bc018abfc
OtterrWater <99858011+OtterrWater@users.noreply.github.com>
2022-03-02 19:13:49 -0800

    Update KasugaiMain.py

--- KasugaiMain.py
+++ KasugaiMain.py
@@ -1,6 +1,6 @@
 import hikari
 
 #DO NOT REVEAL OR SHARE TOKEN
-bot = hikari.GatewayBot(token='OTQ4NzY1ODU2NjM5MzczMzMz.YiAlDw.CsWd2WTfD0FvlDrzXtuJ7oszKBg')
+bot = hikari.GatewayBot(token='InsertTokenHere')
 
 bot.run()
