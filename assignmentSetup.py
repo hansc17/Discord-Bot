02663725938974a08706a7d2e8140a1d3f0d21a3
mhipolito0499 <mhipolito0499@gmail.com>
2022-03-06 13:56:10 -0800

    Error Checking added
    Error checking handled for invalid user input for date and roles

--- assignmentSetup.py
+++ assignmentSetup.py
@@ -1,10 +1,13 @@
-#imports
+#testing
 import hikari
+from hikari import *
 import lightbulb
+from lightbulb import *
 from lock import *
 import psycopg2
+import datetime
 
-#Setting connection to database in AWS RDS
+#Setting connection to database
 conn = psycopg2.connect(
     host=host_name,
     database=database_name,
@@ -19,7 +22,8 @@ cursor = conn.cursor()
 conn.autocommit = True
 
 bot = lightbulb.BotApp(
-    token=TOKEN
+    token=TOKEN,
+    #default_enabled_guilds=(deg)
 )
 
 @bot.listen(hikari.StartedEvent)
@@ -30,10 +34,11 @@ async def on_started(event):
 async def on_ready(event):
     print('I am online!')
 
+
 @bot.command
-@lightbulb.option('duedate', 'Due date of assignment. Format: MM-DD-YYYY (example: 03-01-2022)')
+@lightbulb.option('duedate', 'Due date of assignment. Format: MM-DD-YYYY (example: 03-01-2022)', type=datetime.datetime)
 @lightbulb.option('assignment', 'Assignment name')
-@lightbulb.option('section', 'Class assignment is from. Format: @"class" (example: @comp380) ')
+@lightbulb.option('section', 'Class assignment is from. Format: @"class" (example: @comp380) ', type=hikari.Role)
 @lightbulb.command('addassn', 'Set up a notification for an upcoming assignment.')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def addassn(ctx):
@@ -43,14 +48,29 @@ async def addassn(ctx):
     guildID = ctx.member.guild_id
     userID = ctx.member.id
     username = ctx.member.username
-
-    #store input in database/table
-    query = "INSERT INTO student (guild_ID, user_id, username, section, assignment, due_date) VALUES(%s, %s, %s, %s, %s, %s);"
-    data = (guildID, userID, username, section, assignment, duedate)
-    cursor.execute(query, data)
-    #commit changes
-    conn.commit()
-    await ctx.respond(f'Noted, {assignment} is due on {duedate} for {section}')
+    ######checks for proper input before sending it to database:###########
+    class_id = section.id
+    class_name = section.name
+    role_ids = ctx.member.role_ids
+    #checks if user has assigned role and disallows the user to use @everyone role
+    if class_id not in role_ids:
+        await ctx.respond('You are not assigned to this class! Check if you are assigned to the class and try the command again.')
+    elif class_id == guildID:
+        await ctx.respond('This role is not a valid class!')
+    else:
+        #store user input into database
+        query = "INSERT INTO student (guild_id, user_id, username, section, section_id, assignment, due_date) VALUES(%s, %s, %s, %s, %s, %s, %s);"
+        data = (guildID, userID, username, class_name, class_id, assignment, duedate)
+        #check for invalid dates
+        try: 
+            cursor.execute(query, data)
+        except psycopg2.errors.InvalidDatetimeFormat:
+            print("Invalid Date format!")
+            await ctx.respond(f'{duedate} is not a valid date! Please try the command again with the date format being: MM-DD-YYYY')
+        else:
+            #commit user input to database
+            conn.commit()
+            await ctx.respond(f'Noted, {assignment} is due on {duedate} for {section}')
 
 
 bot.run()
